c918ea4a64ead94025f7ab09f06d027ef9de9ad7
samuelkime267 <samuelkime7@gmail.com>
2025-08-10 20:44:03 +0100

    updated client

--- prisma/generated/prisma/edge.js
+++ prisma/generated/prisma/edge.js
@@ -363,7 +363,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
@@ -373,7 +373,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/prisma/index.js
+++ prisma/generated/prisma/index.js
@@ -364,7 +364,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
@@ -374,7 +374,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
