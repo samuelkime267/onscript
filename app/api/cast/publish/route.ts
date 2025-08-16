2c367ecd10ebf71b260053974cf2a881b444fe7a
samuelkime267 <samuelkime7@gmail.com>
2025-08-16 11:08:28 +0100

    fixed build error

--- app/api/cast/publish/route.ts
+++ app/api/cast/publish/route.ts
@@ -159,3 +159,20 @@
 // //     );
 // //   }
 // // }
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    return NextResponse.json({ message: "Hello, world!" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        error: "Something went wrong",
+      },
+      { status: 400 },
+    );
+  }
+}
